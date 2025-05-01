08a270ebfda76f68c9457c511d6bbb4c2410a261
JoseVillegas9 <mozilla_luffy@hotmail.com>
2025-04-30 22:33:31 -0500

    codigo rama uno

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using practica2.Models;
 
 namespace practica2.Data;
 
@@ -9,4 +10,26 @@ public class ApplicationDbContext : IdentityDbContext
         : base(options)
     {
     }
+    public DbSet<Mascota> Mascotas { get; set; }
+         public DbSet<Adoptante> Adoptantes { get; set; }
+         public DbSet<Adopcion> Adopciones { get; set; }
+ 
+         protected override void OnModelCreating(ModelBuilder modelBuilder)
+         {
+             base.OnModelCreating(modelBuilder);
+ 
+             // 🔥 Relación 1:1 Mascota - Adopcion
+             modelBuilder.Entity<Mascota>()
+                 .HasOne(m => m.Adopcion)
+                 .WithOne(a => a.Mascota)
+                 .HasForeignKey<Adopcion>(a => a.MascotaId)
+                 .OnDelete(DeleteBehavior.Restrict);
+ 
+             // 🔥 Relación 1:N Adoptante - Adopciones
+             modelBuilder.Entity<Adopcion>()
+                 .HasOne(a => a.Adoptante)
+                 .WithMany(adoptante => adoptante.Adopciones)
+                 .HasForeignKey(a => a.AdoptanteId)
+                 .OnDelete(DeleteBehavior.Cascade);
+         }
 }
